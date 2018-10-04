7304774aff67b2aab1d828b1d89aee4e07432a39
Mallory Feuer <mallory.feuer@gmail.com>
2018-10-04 13:40:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+function loadIssue() {
+  let issue = {
+    state: "closed",
+    number: 5,
+    created_at: "2018-03-31 16:23:13 UTC",
+    body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
+  }
+  
+  let template = Handlebars.compile(document.getElementByID("issue-template").innerHTML);
+  let result = template(issue);
+}
